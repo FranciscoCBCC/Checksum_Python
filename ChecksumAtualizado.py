a70f4da3c774f6ded4b9803471d4fc1e216e04fa
FRANCISCO <francisco_nascimento@outlook.com.br>
2016-09-27 00:29:18 -0300

    Foi removido os botoes sair e validar mensagem por causa problemas, porém é necessário inseri-los corretamente no programa.

--- ChecksumAtualizado.py
+++ ChecksumAtualizado.py
@@ -9,14 +9,15 @@ def conta(string):
     return soma
 
 def entradaMensagem():
-    string = input(str("Entre com sua mensagem: "))
+    #string = input(str("Entre com sua mensagem: "))
+    string = entrada.get()
     soma = conta (string)
     print("Checksum: ",soma)
     
 def validarMensagem():
     stringValida = input(str("Entre com sua mensagem para verificacao: "))
     somaValida = int(input("Digite o checksum: "))
-    soma_aux = conta(stringValida)
+    soma_aux = conta(stringValida) #stringValida
     if somaValida != soma_aux:
         print("Messagem diferentes")
     else:
@@ -25,6 +26,8 @@ def validarMensagem():
 def sair():
         print ("Saindo ...")
         sys.exit(0)
+        #janela.main_quit() 
+
 
 
 janela = Tk() #Instancia uma janela
@@ -36,21 +39,9 @@ entrada.pack()
 botao = Button(frame1, text = "Calcular", command = entradaMensagem) #Cria um botão no frame 1
 botao['width'] = 10 #Define a largura do botão
 #botao.bind("", entradaMensagem) #Cria um event dando uma ação ao botão
-botao.pack() #Exibe oo botão
+botao.pack(side = RIGHT) #Exibe oo botão
 
 
-frame2 = Frame(janela) #Cria um frame na janela
-frame2.pack() #Exibe o frame
-botao2 = Button(frame2, text = "Validar", command = validarMensagem) #Cria um botão no frame 1
-botao2['width'] = 10 #Define a largura do botão
-#botao.bind("", entradaMensagem) #Cria um event dando uma ação ao botão
-botao2.pack() #Exibe oo botão
-frame3 = Frame(janela) #Cria um frame na janela
-frame3.pack() #Exibe o frame
-botao3 = Button(frame3, text = "Sair", command = sair) #Cria um botão no frame 1
-botao3['width'] = 10 #Define a largura do botão
-#botao.bind("", entradaMensagem) #Cria um event dando uma ação ao botão
-botao3.pack() #Exibe oo botão
 janela.mainloop() #Loop da janela principal
 
 
