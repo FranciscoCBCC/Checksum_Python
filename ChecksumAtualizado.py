dce6a2602048e8bebeed437a641f7a5e2e9df0b9
Francisco <francisco_nascimento@outlook.com.br>
2016-09-27 10:18:03 -0300

    Foi corrigido os botoes de calcular checksum e validar mensagem, foi inserido caixas de texto para inserção de dados. A aplicação está funcionando normalmente necessitando apenas, caso queira, melhorar o layout.

--- ChecksumAtualizado.py
+++ ChecksumAtualizado.py
@@ -12,16 +12,23 @@ def entradaMensagem():
     #string = input(str("Entre com sua mensagem: "))
     string = entrada.get()
     soma = conta (string)
-    print("Checksum: ",soma)
+    #print("Checksum: ",soma)
+    lb['text'] = 'Checksum: ',soma
+    
     
 def validarMensagem():
-    stringValida = input(str("Entre com sua mensagem para verificacao: "))
-    somaValida = int(input("Digite o checksum: "))
+    #stringValida = input(str("Entre com sua mensagem para verificacao: "))
+    #somaValida = int(input("Digite o checksum: "))
+    stringValida = entradaMsg.get()
+    somaValida = int(entradaChecksum.get())
+    
     soma_aux = conta(stringValida) #stringValida
     if somaValida != soma_aux:
-        print("Messagem diferentes")
+        #print("Messagem diferentes")
+        lb1['text'] = 'Mensagem diferente'
     else:
-        print("Messagem iguais")
+        #print("Messagem iguais")
+        lb1['text'] = 'Mensagem igual'
 
 def sair():
         print ("Saindo ...")
@@ -31,18 +38,32 @@ def sair():
 
 
 janela = Tk() #Instancia uma janela
-janela.title("Checksum")
-frame1 = Frame(janela) #Cria um frame na janela
-frame1.pack() #Exibe o frame
+janela.title("Calcular Checksum")
+janela.geometry("200x200") 
+
+    #Botão De calcular cchecksum
+#frame1 = Frame(janela) #Cria um frame na janela
+#frame1.pack() #Exibe o frame
 entrada = Entry(janela)
 entrada.pack()
-botao = Button(frame1, text = "Calcular", command = entradaMensagem) #Cria um botão no frame 1
+botao = Button(janela, text = "Calcular", command = entradaMensagem) 
 botao['width'] = 10 #Define a largura do botão
 #botao.bind("", entradaMensagem) #Cria um event dando uma ação ao botão
-botao.pack(side = RIGHT) #Exibe oo botão
+botao.pack() #Exibe oo botão
+    #
+    #Botão de validar mensagem
+entradaMsg = Entry(janela)
+entradaMsg.pack()
+entradaChecksum = Entry(janela)
+entradaChecksum.pack()
+botaoValidar = Button(janela, text = "Validar Mensagem", command = validarMensagem) 
+botaoValidar['width'] = 50
+botaoValidar.pack()
+    #
 
+lb = Label(janela, text='Checksum')
+lb.pack()
+lb1 = Label(janela, text='Validação')
+lb1.pack()
 
 janela.mainloop() #Loop da janela principal
-
-
-        
